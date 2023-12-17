c38c0b20dc259b628b92f58a5833617b6365317c
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-17 16:47:17 +0000

    hello world

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const { PORT = 9090 } = process.env;
 const app = express();
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
