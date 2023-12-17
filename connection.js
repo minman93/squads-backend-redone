88c76d31379eb6cbea01959407cb7d0bc9a7fbd5
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-17 21:55:29 +0000

    new

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+const seasons = require("./season-data");
 
 require("dotenv").config({
   path: `${__dirname}/./.env.${ENV}`,
@@ -9,9 +10,51 @@ const config =
   ENV === "production"
     ? { connectionString: process.env.DATABASE_URL, max: 2 }
     : {};
+let pool = new Pool(config);
+const seedData = async () => {
+  const dropSeasons = "DROP TABLE IF EXISTS seasons CASCADE";
+  const createSeasons = `CREATE TABLE seasons (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255)
+      )`;
+
+  await pool.query(dropSeasons);
+
+  await pool.query(createSeasons);
+};
+
+class Season {
+  constructor(season) {
+    this.season = season;
+  }
+
+  static async insertAllSeasons() {
+    for (const season of seasons) {
+      const query = "INSERT INTO seasons (name) VALUES ($1)";
+      const values = [season];
+
+      try {
+        await pool.query(query, values);
+        console.log(`Inserted season: ${season}`);
+      } catch (error) {
+        console.error("Error inserting season:", error);
+      }
+    }
+
+    console.log("All seasons inserted successfully");
+  }
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+(async () => {
+  try {
+    await seedData();
+    await Season.insertAllSeasons();
+  } catch (error) {
+    console.error("Error in main block:", error);
+  }
+})();
 
-module.exports = new Pool(config);
+module.exports = pool(config);
