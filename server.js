8eb29a1811a24c2edf4ac9ab17897e43a26f2941
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-17 16:36:05 +0000

    graphQL removed

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const graphql = require("graphql");
+
 const { PORT = 9090 } = process.env;
 
-const QueryRoot = new graphql.GraphQLObjectType({
-  name: "Query",
-  fields: () => ({
-    hello: {
-      type: graphql.GraphQLString,
-      resolve: () => "Hello world!",
-    },
-  }),
-});
-
-const schema = new graphql.GraphQLSchema({ query: QueryRoot });
-
 const app = express();
-app.use(
-  "/api",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
