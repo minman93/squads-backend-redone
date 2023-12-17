3c3700bf3b47ead2c17452c0553987ed889885e7
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-17 15:36:10 +0000

    installed pg

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const graphql = require("graphql");
+const { Client } = require("pg");
+const client = new Client({
+  host: "localhost",
+  user: "{YOUR_POSTGRES_USERNAME}",
+  password: "{YOUR_POSTGRES_PASSWORD}",
+  database: "{YOUR_POSTGRES_DATABASE}",
+});
+client.connect();
 
 const QueryRoot = new graphql.GraphQLObjectType({
   name: "Query",
@@ -11,3 +19,15 @@ const QueryRoot = new graphql.GraphQLObjectType({
     },
   }),
 });
+
+const schema = new graphql.GraphQLSchema({ query: QueryRoot });
+
+const app = express();
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(3000);
