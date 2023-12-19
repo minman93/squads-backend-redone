11ef2641d8d8a42a964c63536424f758d87f7e1a
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-19 13:16:59 +0000

    files fixed for real now

--- data/js/club-data.js
+++ data/js/club-data.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const clubsFilePath = "../csv/club-data.csv";
+const clubsFilePath = "./csv/club-data.csv";
 
 const readClubsFromCSV = () => {
   return new Promise((resolve, reject) => {
