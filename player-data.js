936b6620a7095e8187c7c8be07a080439dad10ff
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-19 13:25:58 +0000

    djk

--- player-data.js
+++ player-data.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const playersFilePath = "./csv/player-data.csv";
+const playersFilePath = "./player-data.csv";
 
 const readPlayersFromCSV = () => {
   return new Promise((resolve, reject) => {
