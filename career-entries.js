d451a84d643918b16380af8d0c3ed7188851c254
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-19 01:58:49 +0000

    career entries added

---
+++ career-entries.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+const careerEntriesFilePath = "./player-data.csv";
+
+const readCareerEntriesFromCSV = () => {
+  return new Promise((resolve, reject) => {
+    const players = [];
+    fs.createReadStream(careerEntriesFilePath)
+      .pipe(csv())
+      .on("data", (row) => {
+        players.push(row);
+      })
+      .on("end", () => {
+        resolve(players);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+};
+
+const readCareerEntries = async () => {
+  try {
+    const careerEntries = await readCareerEntriesFromCSV();
+    return careerEntries;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+readCareerEntries();
+
+module.exports = readCareerEntries;
