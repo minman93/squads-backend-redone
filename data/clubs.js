26f3fbcd276b81611858a4462d3bcbc4e6722f6c
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-30 13:17:31 +0000

    IDs added

--- data/clubs.js
+++ data/clubs.js
@@ -1,353 +1,365 @@
 module.exports = [
   {
+    id: 1,
     name: "Arsenal",
     badge: "https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png",
     primaryColour: "#EF0107",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 2,
     name: "Aston Villa",
-    badge:
-      "https://seeklogo.com/images/A/aston-villa-logo-55377F01BF-seeklogo.com.png",
+    badge: "https://seeklogo.com/images/A/aston-villa-logo-55377F01BF-seeklogo.com.png",
     primaryColour: "#670E36",
     secondaryColour: "#95BFE5",
   },
   {
+    id: 3,
     name: "Barnsley",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Barnsley_FC.svg/1200px-Barnsley_FC.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Barnsley_FC.svg/1200px-Barnsley_FC.svg.png",
     primaryColour: "#D71921",
     secondaryColour: "#000000",
   },
   {
-    id: "4",
+    id: 4,
     name: "Birmingham City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Birmingham_City_FC_logo.svg/1200px-Birmingham_City_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Birmingham_City_FC_logo.svg/1200px-Birmingham_City_FC_logo.svg.png",
     primaryColour: "#0000FF",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 5,
     name: "Blackburn Rovers",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/0/0f/Blackburn_Rovers.svg/1200px-Blackburn_Rovers.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0f/Blackburn_Rovers.svg/1200px-Blackburn_Rovers.svg.png",
     primaryColour: "#009EE0",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 6,
     name: "Blackpool",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Blackpool_FC_logo.svg/1200px-Blackpool_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Blackpool_FC_logo.svg/1200px-Blackpool_FC_logo.svg.png",
     primaryColour: "#F68712",
     secondaryColour: "#000000",
   },
   {
+    id: 7,
     name: "Bolton Wanderers",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Bolton_Wanderers_FC_logo.svg/1200px-Bolton_Wanderers_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Bolton_Wanderers_FC_logo.svg/1200px-Bolton_Wanderers_FC_logo.svg.png",
     primaryColour: "#FFFFFF",
     secondaryColour: "#00194A",
   },
   {
+    id: 8,
     name: "Bournemouth",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/AFC_Bournemouth_%282013%29.svg/1200px-AFC_Bournemouth_%282013%29.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/AFC_Bournemouth_%282013%29.svg/1200px-AFC_Bournemouth_%282013%29.svg.png",
     primaryColour: "#DA291C",
     secondaryColour: "#000000",
   },
   {
+    id: 9,
     name: "Bradford City",
-    badge:
-      "https://i.pinimg.com/originals/61/e6/b0/61e6b06277214ad2bd43b30ee32453b7.png",
+    badge: "https://i.pinimg.com/originals/61/e6/b0/61e6b06277214ad2bd43b30ee32453b7.png",
     primaryColour: "#fbb800",
     secondaryColour: "#D11524",
   },
   {
+    id: 10,
     name: "Brentford",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/it/2/2e/Brentford_FC_logo.png",
+    badge: "https://upload.wikimedia.org/wikipedia/it/2/2e/Brentford_FC_logo.png",
     primaryColour: "#fbb800",
     secondaryColour: "#000000",
   },
   {
+    id: 11,
     name: "Brighton & Hove Albion",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/1200px-Brighton_%26_Hove_Albion_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/1200px-Brighton_%26_Hove_Albion_logo.svg.png",
     primaryColour: "#0057B8",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 12,
     name: "Burnley",
     badge: "https://1000logos.net/wp-content/uploads/2018/06/Burnley-logo.png",
     primaryColour: "#6C1D45",
     secondaryColour: "#99D6EA",
   },
   {
+    id: 13,
     name: "Cardiff City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Cardiff_City_crest.svg/1200px-Cardiff_City_crest.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Cardiff_City_crest.svg/1200px-Cardiff_City_crest.svg.png",
     primaryColour: "#0070B5",
     secondaryColour: "#D11524",
   },
   {
+    id: 14,
     name: "Charlton Athletic",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/commons/6/6a/CharltonBadge_30Jan2020.png",
+    badge: "https://upload.wikimedia.org/wikipedia/commons/6/6a/CharltonBadge_30Jan2020.png",
     primaryColour: "#d4021d",
     secondaryColour: "#ffffff",
   },
   {
+    id: 15,
     name: "Chelsea",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png",
     primaryColour: "#034694",
     secondaryColour: "#ffffff",
   },
   {
+    id: 16,
     name: "Coventry City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/1/1a/Coventry_City_F.C._logo.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/1/1a/Coventry_City_F.C._logo.png",
     primaryColour: "#059DD9",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 17,
     name: "Crystal Palace",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/a/a2/Crystal_Palace_FC_logo_%282022%29.svg",
+    badge: "https://upload.wikimedia.org/wikipedia/en/a/a2/Crystal_Palace_FC_logo_%282022%29.svg",
     primaryColour: "#1B458F",
     secondaryColour: "#C4122E",
   },
   {
+    id: 18,
     name: "Derby County",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/4/4a/Derby_County_crest.svg",
+    badge: "https://upload.wikimedia.org/wikipedia/en/4/4a/Derby_County_crest.svg",
     primaryColour: "#FFFFFF",
     secondaryColour: "#000000",
   },
   {
+    id: 19,
     name: "Everton",
     badge: "https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg",
     primaryColour: "#003399",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 20,
     name: "Fulham",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/e/eb/Fulham_FC_%28shield%29.svg",
+    badge: "https://upload.wikimedia.org/wikipedia/en/e/eb/Fulham_FC_%28shield%29.svg",
     primaryColour: "#FFFFFF",
     secondaryColour: "#000000",
   },
   {
+    id: 21,
     name: "Huddersfield Town",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/7/7d/Huddersfield_Town_A.F.C._logo.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/7/7d/Huddersfield_Town_A.F.C._logo.png",
     primaryColour: "#0E63AD",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 22,
     name: "Hull City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Hull_City_A.F.C._logo.svg/800px-Hull_City_A.F.C._logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Hull_City_A.F.C._logo.svg/800px-Hull_City_A.F.C._logo.svg.png",
     primaryColour: "#F18A01",
     secondaryColour: "#000000",
   },
   {
+    id: 23,
     name: "Ipswich Town",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Ipswich_Town.svg/1200px-Ipswich_Town.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Ipswich_Town.svg/1200px-Ipswich_Town.svg.png",
     primaryColour: "#003399",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 24,
     name: "Leeds United",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Leeds_United_F.C._logo.svg/1200px-Leeds_United_F.C._logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Leeds_United_F.C._logo.svg/1200px-Leeds_United_F.C._logo.svg.png",
     primaryColour: "#FFFFFF",
     secondaryColour: "#1D428A",
   },
   {
+    id: 25,
     name: "Leicester City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/1200px-Leicester_City_crest.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/1200px-Leicester_City_crest.svg.png",
     primaryColour: "#003090",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 26,
     name: "Liverpool",
     badge: "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
     primaryColour: "#C8102E",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 27,
     name: "Luton Town",
-    badge:
-      "https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/301.png",
+    badge: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/301.png",
     primaryColour: "#F78F1E",
     secondaryColour: "#002D62",
   },
   {
-    id: "28",
+    id: 28,
     name: "Manchester City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png",
     primaryColour: "#6CABDD",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 29,
     name: "Manchester United",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png",
     primaryColour: "#DA291C",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 30,
     name: "Middlesbrough",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/sco/thumb/2/2c/Middlesbrough_FC_crest.svg/1200px-Middlesbrough_FC_crest.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/sco/thumb/2/2c/Middlesbrough_FC_crest.svg/1200px-Middlesbrough_FC_crest.svg.png",
     primaryColour: "#E21A23",
     secondaryColour: "#000000",
   },
   {
+    id: 31,
     name: "Newcastle United",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Newcastle_United_Logo.svg/1200px-Newcastle_United_Logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Newcastle_United_Logo.svg/1200px-Newcastle_United_Logo.svg.png",
     primaryColour: "#000000",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 32,
     name: "Norwich City",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Norwich_City_FC_logo.svg/640px-Norwich_City_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Norwich_City_FC_logo.svg/640px-Norwich_City_FC_logo.svg.png",
     primaryColour: "#FFF200",
     secondaryColour: "#00A650",
   },
   {
+    id: 33,
     name: "Nottingham Forest",
-    badge:
-      "https://plprimarystars.scdn7.secure.raxcdn.com/media/view/images/clubs/201819_Season/Forest_crest.png",
+    badge: "https://plprimarystars.scdn7.secure.raxcdn.com/media/view/images/clubs/201819_Season/Forest_crest.png",
     primaryColour: "#DD0000",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 34,
     name: "Oldham Athletic",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Oldham_Athletic_AFC_%28emblem%29.svg/800px-Oldham_Athletic_AFC_%28emblem%29.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Oldham_Athletic_AFC_%28emblem%29.svg/800px-Oldham_Athletic_AFC_%28emblem%29.svg.png",
     primaryColour: "#1D5BA4",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 35,
     name: "Portsmouth",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/3/38/Portsmouth_FC_logo.svg/1200px-Portsmouth_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/3/38/Portsmouth_FC_logo.svg/1200px-Portsmouth_FC_logo.svg.png",
     primaryColour: "#004494",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 36,
     name: "Queens Park Rangers",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/3/31/Queens_Park_Rangers_crest.svg",
+    badge: "https://upload.wikimedia.org/wikipedia/en/3/31/Queens_Park_Rangers_crest.svg",
     primaryColour: "#1D5BA4",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 37,
     name: "Reading",
     badge: "https://upload.wikimedia.org/wikipedia/en/1/11/Reading_FC.svg",
     primaryColour: "#004494",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 38,
     name: "Sheffield United",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Sheffield_United_FC_logo.svg/1200px-Sheffield_United_FC_logo.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Sheffield_United_FC_logo.svg/1200px-Sheffield_United_FC_logo.svg.png",
     primaryColour: "#EE2737",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 39,
     name: "Sheffield Wednesday",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Sheffield_Wednesday_badge.svg/1200px-Sheffield_Wednesday_badge.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Sheffield_Wednesday_badge.svg/1200px-Sheffield_Wednesday_badge.svg.png",
     primaryColour: "#0067B4",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 40,
     name: "Southampton",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/FC_Southampton.svg/1200px-FC_Southampton.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/FC_Southampton.svg/1200px-FC_Southampton.svg.png",
     primaryColour: "#D71920",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 41,
     name: "Stoke City",
     badge: "https://upload.wikimedia.org/wikipedia/en/2/29/Stoke_City_FC.svg",
     primaryColour: "#E03A3E",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 42,
     name: "Sunderland",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Logo_Sunderland.svg/1200px-Logo_Sunderland.svg.png",
+    badge: "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Logo_Sunderland.svg/1200px-Logo_Sunderland.svg.png",
     primaryColour: "#DD0000",
     secondaryColour: "#FFFFFF",
   },
   {
+    id: 43,
     name: "Swansea City",
-    badge:
-      "https://i2-prod.walesonline.co.uk/incoming/article24182057.ece/ALTERNATES/s1200c/0_Swansea-City-22-23-Badge-Blue.png",
+    badge: "https://i2-prod.walesonline.co.uk/incoming/article24182057.ece/ALTERNATES/s1200c/0_Swansea-City-22-23-Badge-Blue.png",
     primaryColour: "#FFFFFF",
     secondaryColour: "#000000",
   },
-  {
-    name: "Swindon Town",
-    badge: "https://upload.wikimedia.org/wikipedia/en/a/a3/Swindon_Town_FC.svg",
-    primaryColour: "#dc161b",
-    secondaryColour: "#ffffff",
-  },
-  {
-    name: "Tottenham Hotspur",
-    badge:
-      "https://www.toptacular.co.uk/wp-content/uploads/2019/09/Tottenham-Badge-200mm-Wafer-Cake.png",
-    primaryColour: "#FFFFFF",
-    secondaryColour: "#132257",
-  },
-  {
-    name: "Watford",
-    badge: "https://upload.wikimedia.org/wikipedia/en/e/e2/Watford.svg",
-    primaryColour: "#FFF200",
-    secondaryColour: "#11210C",
-  },
-  {
-    name: "West Bromwich Albion",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/West_Bromwich_Albion.svg/1200px-West_Bromwich_Albion.svg.png",
-    primaryColour: "#122F67",
-    secondaryColour: "#FFFFFF",
-  },
-  {
-    name: "West Ham United",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/West_Ham_United_FC_logo.svg/1200px-West_Ham_United_FC_logo.svg.png",
-    primaryColour: "#7A263A",
-    secondaryColour: "#1BB1E7",
-  },
-  {
-    name: "Wigan Athletic",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Wigan_Athletic.svg/1200px-Wigan_Athletic.svg.png",
-    primaryColour: "#1D5BA4",
-    secondaryColour: "#FFFFFF",
-  },
-  {
-    name: "Wimbledon",
-    badge: "https://upload.wikimedia.org/wikipedia/en/b/bc/Wimbledon_fc.png",
-    primaryColour: "#122F67",
-    secondaryColour: "#FBEE23",
-  },
-  {
-    name: "Wolverhampton Wanderers",
-    badge:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Wolverhampton_Wanderers.svg/1200px-Wolverhampton_Wanderers.svg.png",
-    primaryColour: "#FDB913",
-    secondaryColour: "#231F20",
-  },
-];
+    {
+      id: 44,
+      name: "Swindon Town",
+      badge: "https://upload.wikimedia.org/wikipedia/en/a/a3/Swindon_Town_FC.svg",
+      primaryColour: "#dc161b",
+      secondaryColour: "#ffffff",
+    },
+    {
+      id: 45,
+      name: "Tottenham Hotspur",
+      badge:
+        "https://www.toptacular.co.uk/wp-content/uploads/2019/09/Tottenham-Badge-200mm-Wafer-Cake.png",
+      primaryColour: "#FFFFFF",
+      secondaryColour: "#132257",
+    },
+    {
+      id: 46,
+      name: "Watford",
+      badge: "https://upload.wikimedia.org/wikipedia/en/e/e2/Watford.svg",
+      primaryColour: "#FFF200",
+      secondaryColour: "#11210C",
+    },
+    {
+      id: 47,
+      name: "West Bromwich Albion",
+      badge:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/West_Bromwich_Albion.svg/1200px-West_Bromwich_Albion.svg.png",
+      primaryColour: "#122F67",
+      secondaryColour: "#FFFFFF",
+    },
+    {
+      id: 48,
+      name: "West Ham United",
+      badge:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/West_Ham_United_FC_logo.svg/1200px-West_Ham_United_FC_logo.svg.png",
+      primaryColour: "#7A263A",
+      secondaryColour: "#1BB1E7",
+    },
+    {
+      id: 49,
+      name: "Wigan Athletic",
+      badge:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Wigan_Athletic.svg/1200px-Wigan_Athletic.svg.png",
+      primaryColour: "#1D5BA4",
+      secondaryColour: "#FFFFFF",
+    },
+    {
+      id: 50,
+      name: "Wimbledon",
+      badge: "https://upload.wikimedia.org/wikipedia/en/b/bc/Wimbledon_fc.png",
+      primaryColour: "#122F67",
+      secondaryColour: "#FBEE23",
+    },
+    {
+      id: 51,
+      name: "Wolverhampton Wanderers",
+      badge:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Wolverhampton_Wanderers.svg/1200px-Wolverhampton_Wanderers.svg.png",
+      primaryColour: "#FDB913",
+      secondaryColour: "#231F20",
+    },
+  ];
   
