f744817f4b10735daebfbe699986f840cde295ff
minman93 <matt_j_inman@hotmail.co.uk>
2023-12-30 14:29:31 +0000

    full Arsenal 2003/2004 season entry

--- data/career-entries.js
+++ data/career-entries.js
@@ -1,20 +1,281 @@
 module.exports = [
   {
     id: 1,
-    player_id: 3,
-    squad_number: 10,
+    player_id: 19,
+    squad_number: 1,
     club_id: 1,
     season_id: 12,
     image_url:
-      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTqgfOB304R7EPCfF3ehqzDuAd9exXgzdF39ljVwvDUsG6nmLnjYC6UJOzYtFaErfrn86Hs3R3hopAfErA",
+      "https://www.arsenalpics.com/p/5/jens-lehmann-arsenal-ajax-0-1-arsenal-96746.jpg.webp",
   },
   {
     id: 2,
-    player_id: 6,
+    player_id: 7,
     squad_number: 3,
     club_id: 1,
     season_id: 12,
     image_url:
       "https://i2-prod.mirror.co.uk/incoming/article10072481.ece/ALTERNATES/s1200c/Ashley-Cole-Arsenal-May-2005.jpg",
+  },
+  {
+    id: 3,
+    player_id: 34,
+    squad_number: 4,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://static.independent.co.uk/2021/10/15/22/4562c52e4ee6e80bbc2f88fbc3f6e2bfY29udGVudHNlYXJjaGFwaSwxNjM0NDAzMzUy-2.19354957.jpg",
+  },
+  {
+    id: 4,
+    player_id: 18,
+    squad_number: 5,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 5,
+    player_id: 24,
+    squad_number: 7,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 6,
+    player_id: 20,
+    squad_number: 8,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 7,
+    player_id: 25,
+    squad_number: 9,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 8,
+    player_id: 3,
+    squad_number: 10,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 9,
+    player_id: 35,
+    squad_number: 11,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 10,
+    player_id: 11,
+    squad_number: 12,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 11,
+    player_id: 14,
+    squad_number: 14,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 12,
+    player_id: 10,
+    squad_number: 17,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 13,
+    player_id: 9,
+    squad_number: 18,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 14,
+    player_id: 13,
+    squad_number: 19,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 15,
+    player_id: 6,
+    squad_number: 22,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 16,
+    player_id: 4,
+    squad_number: 23,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 17,
+    player_id: 17,
+    squad_number: 25,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 18,
+    player_id: 36,
+    squad_number: 27,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 19,
+    player_id: 33,
+    squad_number: 28,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 20,
+    player_id: 1,
+    squad_number: 30,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 21,
+    player_id: 22,
+    squad_number: 32,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 22,
+    player_id: 31,
+    squad_number: 33,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 23,
+    player_id: 2,
+    squad_number: 39,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 24,
+    player_id: 16,
+    squad_number: 45,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 25,
+    player_id: 27,
+    squad_number: 51,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 26,
+    player_id: 30,
+    squad_number: 52,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 27,
+    player_id: 37,
+    squad_number: 53,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 28,
+    player_id: 21,
+    squad_number: 54,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 29,
+    player_id: 28,
+    squad_number: 55,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 30,
+    player_id: 29,
+    squad_number: 56,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
+  },
+  {
+    id: 31,
+    player_id: 5,
+    squad_number: 57,
+    club_id: 1,
+    season_id: 12,
+    image_url:
+      "https://www.thesun.co.uk/wp-content/uploads/2018/03/nintchdbpict000003283749.jpg",
   },
 ];
